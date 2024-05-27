5329dd3351b6a77d5751c8a9629d70d36404c49c
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-05-27 12:39:59 -0700

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let len = array.length;
+
+    for(let target of args){
+        for(let i = 0; i < len; i++){
+            let index = array.indexOf(target);
+            if(index >= 0){
+                array.splice(index,1);
+            }
+            
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
