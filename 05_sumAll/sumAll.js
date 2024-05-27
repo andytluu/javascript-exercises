03330ee97f67c6641a109e0fa0d62d06f48b96b3
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-05-27 13:02:44 -0700

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
+    let start = firstNum;
+    let end = secondNum;
 
+    if(firstNum > secondNum){
+        start = secondNum;
+        end = firstNum;
+    }
+
+    if(firstNum < 0 || secondNum < 0){
+        return "ERROR";
+    }
+
+    if(typeof firstNum != "number" || typeof secondNum != "number"){
+        return "ERROR";
+    }
+    
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
