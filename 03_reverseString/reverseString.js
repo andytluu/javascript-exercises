8b73d20e54cbdc6f8213c0fd55a7c6289bbb636f
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-05-26 20:12:34 -0700

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rString = "";
+    let len = string.length;
+    console.log(len);
+    for (let i = len - 1; i >= 0; i--){
+        rString += string[i];
+    }
+    return rString;
 };
 
 // Do not edit below this line
