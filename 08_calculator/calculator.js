759f46a2ddd37d773657c7985430ff73490763ca
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-14 11:38:10 -0700

    Finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const result = arr.reduce((sum, num) => {
+    return sum + num;
+  }, 0);
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const result = arr.reduce((mult, num) => {
+    return mult * num;
+  });
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  let result = 1;
+	for(let i = 0; i < exp; i++){
+    result = result * num;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  if(num === 0){
+    return 1;
+  }
+  for(let i = 1; i <= num; i++){
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
