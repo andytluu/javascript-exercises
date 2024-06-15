232b1e66e3b0dbca1bf874a0a5a5e993e064844e
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-14 17:35:37 -0700

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(len) {
+    let sum = 1;
+    let prev = 0;
+    let temp = prev;
 
+    if(len == 0){
+        return 0;
+    }
+
+    if(len < 0){
+        return "OOPS";
+    }
+    for(let i = 1; i < len; i++){
+        temp = sum;
+        sum = sum + prev;
+        prev = temp;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
