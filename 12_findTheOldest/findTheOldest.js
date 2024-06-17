8177d4653231ac58105c3a248921c193b546c03d
Andy Luue <155850018+andytluu@users.noreply.github.com>
2024-06-16 17:34:03 -0700

    Finished exercises 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    const oldest = obj.map((person) => {
+        let compare = {};
+        let age = 0;
+        if(!("yearOfDeath" in person)){
+            let today = new Date();
+            let currentYear = today.getFullYear();
+            age = currentYear - person.yearOfBirth;
+        }
+        else{
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        compare["name"] = person.name;
+        compare["year"] = age;
+        return compare;
+    });
 
+    oldest.sort(function (a,b) {
+        return a.year - b.year;
+    });
+
+    old = oldest[oldest.length - 1];
+    return old;
 };
 
 // Do not edit below this line
